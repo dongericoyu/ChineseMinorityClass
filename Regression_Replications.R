a9287098ef78750e5957179285b7ff57d24f061a
dongericoyu <dong-yu@uiowa.edu>
2017-03-27 11:27:54 -0500

    (1)new subsets of Xinjiang and Guangxi; regressions without interactions; (2)finalize tables and figures

--- Regression_Replications.R
+++ Regression_Replications.R
@@ -52,10 +52,10 @@ reordered_vars <- c("job_class", "edu_1", "income_quantile", "age_1", "male_1", 
 
 
 # Generate a tidy data frame
-m1234_df <- rbind(tidy(retro_HM_China) %>% mutate(model = "retro_HM_China"),      # tidy results &
-                  tidy(retro_HU_China) %>% mutate(model = "retro_HU_China"),      # add a variable to
-                  tidy(retro_HZ_China) %>% mutate(model = "retro_HZ_China"),
-                  tidy(retro_UZ_China) %>% mutate(model = "retro_UZ_China")) %>%  # identify model.
+m1234_df <- rbind(tidy(retro_HM_China) %>% mutate(model = "Han v.s. Minorities"),      # tidy results &
+                  tidy(retro_HU_China) %>% mutate(model = "Han v.s. Uyghur"),      # add a variable to
+                  tidy(retro_HZ_China) %>% mutate(model = "Han v.s. Zhuang"),
+                  tidy(retro_UZ_China) %>% mutate(model = "Uyghur v.s. Zhuang")) %>%  # identify model.
   by_2sd(mtcars) %>%                                        # rescale coefficients
   mutate(term = factor(term, levels = reordered_vars)) %>%  # make term a factor &
   group_by(model) %>% arrange(term) %>%                     # reorder
@@ -82,7 +82,7 @@ retro_set1_group<-dwplot(m1234_df, dodge_soze = 1) +
                    "Uyghur v.s. Han", "Zhuang v.s. Han", "Uyghur v.s. Zhuang")) +
   theme_bw() + xlab("Coefficient Estimate") + ylab("") +
   geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
-  ggtitle("Figure 1. Predicting Retrospective Mobility") +
+  ggtitle("Figure 4. Predicting Retrospective Mobility") +
   theme(plot.title = element_text(face="bold"),
         legend.justification=c(0, 0), legend.position=c(0, 0),
         legend.background = element_rect(colour="grey80"),
@@ -134,10 +134,10 @@ reordered_vars_pros<- c("job_class", "edu_1", "income_quantile", "age_1", "male_
 
 
 # Generate a tidy data frame
-m5678_df <- rbind(tidy(prosp_HM_China) %>% mutate(model = "prosp_HM_China"),      # tidy results &
-                  tidy(prosp_HU_China) %>% mutate(model = "prosp_HU_China"),      # add a variable to
-                  tidy(prosp_HZ_China) %>% mutate(model = "prosp_HZ_China"),
-                  tidy(prosp_UZ_China) %>% mutate(model = "prosp_UZ_China")) %>%  # identify model.
+m5678_df <- rbind(tidy(prosp_HM_China) %>% mutate(model = "Han v.s. Minorities"),      # tidy results &
+                  tidy(prosp_HU_China) %>% mutate(model = "Han v.s. Uyghur"),      # add a variable to
+                  tidy(prosp_HZ_China) %>% mutate(model = "Han v.s. Zhuang"),
+                  tidy(prosp_UZ_China) %>% mutate(model = "Uyghur v.s. Zhuang")) %>%  # identify model.
   by_2sd(mtcars) %>%                                        # rescale coefficients
   mutate(term = factor(term, levels = reordered_vars)) %>%  # make term a factor &
   group_by(model) %>% arrange(term) %>%                     # reorder
@@ -164,7 +164,7 @@ prosp_set1_group<-dwplot(m5678_df, dodge_soze = 1) +
                    "Uyghur v.s. Han", "Zhuang v.s. Han", "Uyghur v.s. Zhuang")) +
   theme_bw() + xlab("Coefficient Estimate") + ylab("") +
   geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
-  ggtitle("Figure 2. Predicting prospspective Mobility") +
+  ggtitle("Figure 5. Predicting Prospspective Mobility") +
   theme(plot.title = element_text(face="bold"),
         legend.justification=c(0, 0), legend.position=c(0, 0),
         legend.background = element_rect(colour="grey80"),
@@ -193,95 +193,69 @@ table2<-stargazer(prosp_HM_China, prosp_HU_China, prosp_HZ_China,prosp_UZ_China,
 
 #basic model sets 3.Retrospective and Prospective Mobility- ii.	Xinjiang – just Han/Uyghur (Wu & Song 2014 check) iii.	Guangxi – just Zhuang/Han 
 
-#First, sub-sampling
+#First, sub-sampling: first just Han and the other ethnicity, second controlling for region
 data_HU <- read_dta("C:/Users/Dong/Desktop/Ongoing Project-R/ChineseMinorityClass-MPSA2017/ChineseMinorityClass/individual_data_for_R(HU).dta")
+
+HU_Xinjiang <- read_dta("C:/Users/Dong/Desktop/Ongoing Project-R/ChineseMinorityClass-MPSA2017/ChineseMinorityClass/HU_Xinjiang.dta")
 
 data_HZ <- read_dta("C:/Users/Dong/Desktop/Ongoing Project-R/ChineseMinorityClass-MPSA2017/ChineseMinorityClass/individual_data_for_R(HZ).dta")
 
+HZ_Guangxi <- read_dta("C:/Users/Dong/Desktop/Ongoing Project-R/ChineseMinorityClass-MPSA2017/ChineseMinorityClass/HZ_Guangxi.dta")
+
+
 #3(1)Han v.s. Uyghur - retros
-retro_HU_XJ <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Uyghur*Xinjiang, data = data_HU)
-dwplot(retro_HU_XJ)
 
-interplot(m=retro_HU_XJ, var1="Uyghur", var2 = "Xinjiang") +
-  xlab('Uyghur') +
+
+#3.22: creating a new table to present the result first; then draw
+retro_HU_XJ <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han, data = HU_Xinjiang)
+
+#just for robust check
+retro_HU <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han+Xinjiang + Han*Xinjiang, data = data_HU)
+
+
+prosp_HU_XJ <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han, data = HU_Xinjiang)
+prosp_HU <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han+Xinjiang + Han*Xinjiang, data = data_HU)
+
+
+
+retro_HZ_GX <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han, data = HZ_Guangxi)
+retro_HZ <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han+Guangxi + Han*Guangxi, data = data_HZ)
+
+prosp_HZ_GX <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han, data = HZ_Guangxi)
+prosp_HZ <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Han+Guangxi + Han*Guangxi, data = data_HZ)
+
+
+#table
+table3<-stargazer(retro_HU_XJ, retro_HZ_GX, prosp_HU_XJ, prosp_HZ_GX, title="Regression Results of Controlling Region",
+                  align=TRUE, dep.var.labels=c("Han v.s. Uyghur(Retro)","Han v.s. Zhuang(Retro)", "Han v.s. Uyghur(Prosp)", "Han v.s. Zhuang(Prosp)" ),
+                  covariate.labels=c("Occupation_class", "Education", "Income", 
+                                     "Age", "Male", "CCP members", "Urban Hukou", "Local Hukou", 
+                                     "Han"),
+                  omit.stat=c("LL","ser","f"), no.space=TRUE)
+
+#for reference: table 4, interacting with region
+table4<-stargazer(retro_HU, retro_HZ, prosp_HU, prosp_HZ, title="Regression Results of Controlling Region",
+                  align=TRUE, dep.var.labels=c("Han v.s. Uyghur(Retro)","Han v.s. Zhuang(Retro)", "Han v.s. Uyghur(Prosp)", "Han v.s. Zhuang(Prosp)" ),
+                  covariate.labels=c("Occupation_class", "Education", "Income", 
+                                     "Age", "Male", "CCP members", "Urban Hukou", "Local Hukou", 
+                                     "Han", "Xinjiang", "Han:Xinjiang", "Guangxi", "Han:Guangxi"),
+                  omit.stat=c("LL","ser","f"), no.space=TRUE)
+
+
+#plotting interplot for Guangxi
+interplot(m=prosp_HU, var1="Han", var2 = "Xinjiang") +
+  xlab('Han') +
   ylab('Estimated Coefficient for Xinjiang') +
-  ggtitle('Estimated Coefficient of Xinjiang on Retrospective Mobility by Race (Uyghur)') +
+  ggtitle('Estimated Coefficient of Xinjiang on Prospective Mobility by Ethinicity (Han)') +
   theme(plot.title = element_text(face='bold'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#3(2)Han v.s. Uyghur - prosp
-prosp_HU_XJ <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Uyghur + Xinjiang + Uyghur*Xinjiang, data = data_HU)
-interplot(m=prosp_HU_XJ, var1="Uyghur", var2 = "Xinjiang") +
-  xlab('Uyghur') +
-  ylab('Estimated Coefficient for Xinjiang') +
-  ggtitle('Estimated Coefficient of Xinjiang on Prospective Mobility by Race (Uyghur)') +
-  theme(plot.title = element_text(face='bold'))
-
-
-#3(3)Han v.s. Zhuang - retros
-retro_HZ_GX <- glm(retro_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Zhuang + Guangxi + Zhuang*Guangxi, data = data_HZ)
-interplot(m=retro_HZ_GX, var1="Zhuang", var2 = "Guangxi") +
-  xlab('Zhuang') +
-  ylab('Estimated Coefficient for Guangxi') +
-  ggtitle('Estimated Coefficient of Guangxi on Retrospective Mobility by Race(Zhuang)') +
-  theme(plot.title = element_text(face='bold'))
-
-#3(4)Han v.s. Zhuang - prosp
-prosp_HZ_GX <- glm(prosp_mobility_1 ~ job_class + edu_1 + income_quantile + age_1 + male_1 + ccp_1 + current_urban_hukou_1 + local_hukou_1 + Zhuang + Guangxi + Zhuang*Guangxi, data = data_HZ)
-interplot(m=prosp_HZ_GX, var1="Zhuang", var2 = "Guangxi") +
-  xlab('Zhuang') +
-  ylab('Estimated Coefficient for Guangxi') +
-  ggtitle('Estimated Coefficient of Guangxi on Retrospective Mobility by Race(Zhuang)') +
-  theme(plot.title = element_text(face='bold'))
 
 #plotting the  coefficients of model
-set3_group_1<-dwplot(retro_HU_XJ)
-
-#groupped IVS 
-# Define order for predictors that can be grouped
-reordered_vars_set3(1)<- c("job_class", "edu_1", "income_quantile", "age_1", "male_1", "ccp_1", "current_urban_hukou_1",
-                        "local_hukou_1","Uyghur_Han", "Xinjiang","Uyghur*Xinjiang")
-
-
-
-
 # Generate a tidy data frame
-mset3_df_1<- rbind(tidy(retro_HU_XJ) %>% mutate(model = "retro_HU_XJ"),      # tidy results &
-                  tidy(prosp_HU_XJ) %>% mutate(model = "prosp_HU_XJ")) %>%  # identify model.
+mset3_df_1<- rbind(tidy(retro_HU_XJ) %>% mutate(model = "Han_Uyghur(retro)"),      # tidy results &
+                   tidy(retro_HZ_GX) %>% mutate(model = "Han_Zhuang(retro)"),
+                   tidy(prosp_HU_XJ) %>% mutate(model = "Han_Uyghur(prosp)"),
+                   tidy(prosp_HZ_GX) %>% mutate(model = "Han_Zhuang(prosp)")) %>%  # identify model.
   by_2sd(mtcars) %>%                                        # rescale coefficients
   mutate(term = factor(term, levels = reordered_vars_set3)) %>%  # make term a factor &
   group_by(model) %>% arrange(term) %>%                     # reorder
@@ -293,36 +267,33 @@ mset3_df_1<- rbind(tidy(retro_HU_XJ) %>% mutate(model = "retro_HU_XJ"),      # t
                        ccp_1 = "CCP members",
                        current_urban_hukou_1 = "Urban Hukou",
                        local_hukou_1 = "Local Hukou",
-                       Uyghur_Han = "Uyghur v.s. Han",
-                       Xinjiang = "Xinjiang",
-                       Uyghur*Xinjiang = "Uyghur*Xinjiang"))
+                       Han = "Han"))
 
-mset3_df_1
+View(mset3_df_1)
+#for somehow, the dataframe cannot display Han Chinese: thus I edit it in STATA, and read it again
+
 
 
 # Save finalized plot to an object 
 set3_group_1<-dwplot(mset3_df_1, dodge_soze = 1) + 
   relabel_y_axis(c("Occupation_class", "Education", "Income", 
                    "Age", "Male", "CCP members", "Urban Hukou", "Local Hukou",
-                   "Uyghur v.s. Han", "Xinjiang","Uyghur*Xinjiang" )) +
+                   "Han" )) +
   theme_bw() + xlab("Coefficient Estimate") + ylab("") +
   geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
-  ggtitle("Figure 3. Uyghur comparing to Han, with Regional Control") +
+  ggtitle("Figure 6. Han comparing with Uyghur and Zhuang, with Regional Control") +
   theme(plot.title = element_text(face="bold"),
         legend.justification=c(0, 0),
         legend.background = element_rect(colour="grey80"),
         legend.title = element_blank()) 
 
 # Create list of brackets (label, topmost included predictor, bottommost included predictor)
-four_brackets_set3_1 <- list( c("SES", "Occupation_class", "Income"), 
-                            c("Demographics", "Age", "CCP members"),
-                            c("Hukou Status", "Urban Hukou", "Local Hukou"),
-                            c("Ethnicity and Region", "Uyghur v.s. Han", "Uyghur*Xinjiang"))
 
-set3_gplot_1 <- set3_group_1 %>% add_brackets(four_brackets_set3_1)
+
+set3_gplot_1 <- set3_group_1 
 
 # to save to file (not run)
-grid.arrange(set3_gplot_1)    # to display
+grid.arrange(set3_group_1)    # to display
 
 #creating table
 table3<-stargazer(retro_HU_XJ, prosp_HU_XJ, retro_HZ_GX, prosp_HZ_GX, title="Regression Results of Uyghur and Zhuang comparing to Han",
@@ -356,10 +327,10 @@ reordered_vars_set4<- c("job_class", "edu_1", "income_quantile", "age_1", "male_
 
 
 # Generate a tidy data frame
-mset4_df <- rbind(tidy(class_HM) %>% mutate(model = "class_HM"),      # tidy results &
-                  tidy(class_HU) %>% mutate(model = "class_HU"),      # add a variable to
-                  tidy(class_HZ) %>% mutate(model = "class_HZ"),
-                  tidy(class_UZ) %>% mutate(model = "class_UZ")) %>%  # identify model.
+mset4_df <- rbind(tidy(class_HM) %>% mutate(model = "Han v.s. minorities"),      # tidy results &
+                  tidy(class_HU) %>% mutate(model = "Han v.s. Uyghur"),      # add a variable to
+                  tidy(class_HZ) %>% mutate(model = "Han v.s. Zhuang"),
+                  tidy(class_UZ) %>% mutate(model = "Uyghur v.s. Zhuang")) %>%  # identify model.
   by_2sd(mtcars) %>%                                        # rescale coefficients
   mutate(term = factor(term, levels = reordered_vars_set4)) %>%  # make term a factor &
   group_by(model) %>% arrange(term) %>%                     # reorder
@@ -386,7 +357,7 @@ class_set4_group<-dwplot(mset4_df, dodge_soze = 1) +
                    "Uyghur v.s. Han", "Zhuang v.s. Han", "Uyghur v.s. Zhuang")) +
   theme_bw() + xlab("Coefficient Estimate") + ylab("") +
   geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
-  ggtitle("Figure 4. Self-perception of Social Class") +
+  ggtitle("Figure 6. Self-perception of Social Class") +
   theme(plot.title = element_text(face="bold"),
         legend.justification=c(0, 0),
         legend.background = element_rect(colour="grey80"),
